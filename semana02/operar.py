711399bdba48596d654d2c2d33e405730f46b56d
cpulachev8 <cpulachev8@gmail.com>
2021-10-20 22:40:01 -0300

    Sem2: Ejecución de calculadora

--- semana02/operar.py
+++ semana02/operar.py
@@ -3,11 +3,11 @@
 
 import calculadora
 
-calculadora.sumar(20, 4)
-calculadora.restar(20,4)
-calculadora.multiplicar(20,4)
-calculadora.dividir(20,4)
+calculadora.imprimir("suma", calculadora.sumar(20, 4))
+calculadora.imprimir("resta", calculadora.restar(20,4))
+calculadora.imprimir("multiplicación", calculadora.multiplicar(20,4))
+calculadora.imprimir("división", calculadora.dividir(20,4))
 
 # Resolver (3 + 5)*10
 val1 = calculadora.sumar(3, 5)
-calculadora.multiplicar(val1, 10)
+calculadora.imprimir("operación", calculadora.multiplicar(val1, 10))
