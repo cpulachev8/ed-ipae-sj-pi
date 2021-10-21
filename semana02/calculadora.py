da42259edb52f5dd6406334cad5a9956733fd007
cpulachev8 <cpulachev8@gmail.com>
2021-10-20 23:33:02 -0300

    Sem2: Formas de importar módulos

--- semana02/calculadora.py
+++ semana02/calculadora.py
@@ -1,3 +1,5 @@
+from math import pow
+
 def sumar(a, b):
     return a + b
 
@@ -10,6 +12,9 @@ def multiplicar(a, b):
 def dividir(a, b):
     return a / b
 
+def potencia(a, b):
+    return pow(5, 2)
+
 def fib(n):
     a, b, c = 0, 1, 0
     while b < n:
@@ -20,5 +25,8 @@ def fib(n):
        a, b = b, a + b
     print()
 
+# Para formatear:
+# %s texto o cadena
+# %d decimales o números
 def imprimir(operacion, resultado):
     print("La %s es: %d"% (operacion, resultado))
--- semana02/operar.py
+++ semana02/operar.py
@@ -9,5 +9,10 @@ calculadora.imprimir("multiplicación", calculadora.multiplicar(20,4))
 calculadora.imprimir("división", calculadora.dividir(20,4))
 
 # Resolver (3 + 5)*10
-val1 = calculadora.sumar(3, 5)
-calculadora.imprimir("operación", calculadora.multiplicar(val1, 10))
+calculadora.imprimir("operación", calculadora.multiplicar(calculadora.sumar(3, 5), 10))
+calculadora.imprimir("Potencia", calculadora.potencia(5,2))
+
+# Tarea: Crear el archivo operacion_combinada.py que permita leer una cadena que representa los cálculos que se deben realizar con el módulo de calculadora. Mínimo debe permitir trabajar con 3 números. Debe imprimir el resultado de la operación
+# Por ejemplo: 4+5*10 = 54
+# En caso se ingrese una cadena que no se puede operar imprimir un mensaje de error. P.e. +9*5-
+# Plazo hasta el lunes
