bb06c9837d01704f8425bf99496420787050759b
cpulachev8 <cpulachev8@gmail.com>
2021-11-24 22:09:57 -0300

    sem7: POO

---
+++ semana07/Persona.py
@@ -0,0 +1,30 @@
+class Persona:
+    
+    def __init__(self) -> None:
+        self.nombre = input("Ingrese nombre: ")
+        self.edad = int(input("Ingrese edad: "))
+        self.estado_civil = input("Ingrese estado civil (S|C|V|D)")
+
+    def es_mayor_edad(self) -> bool:
+        # if self.edad >= 18:
+        #     return True
+        # else:
+        #     return False
+        # Un return corta el programa para devolver un valor
+        # if self.edad >= 18:
+        #     return True
+        # return False
+        return self.edad >= 18
+    
+    def imprimir_datos(self) -> None:
+        print("Nombre: ", self.nombre)
+        print("Edad: ", self.edad)
+
+    def imprimir_mayor_edad(self) -> None:
+        if self.es_mayor_edad():
+            print("Es mayor de edad")
+        else:
+            print("No es mayor de edad")
+
+    # definir un método que indique si puede tramitar licencia de conducir
+    # Una persona puede tramita una licencia si es mayor de edad o es casado y tiene 16 años
---
+++ semana07/intro.py
@@ -0,0 +1,18 @@
+# Repaso de métodos propios de python
+# cadena = "1,2,3,4,5"
+# list = cadena.split(",")
+# print(cadena)
+# print(list)
+
+# uso del método __init__ de una clase
+from Persona import Persona
+
+print("****** PERSONA UNO **********")
+persona1 = Persona()
+persona1.imprimir_datos()
+persona1.imprimir_mayor_edad()
+
+print("****** PERSONA DOS **********")
+persona2 = Persona()
+persona2.imprimir_datos()
+persona2.imprimir_mayor_edad()
