b8d17bcd4990fbd2b09fc1e52a0edba0d6d5fe7b
cpulachev8 <cpulachev8@gmail.com>
2021-12-18 20:43:07 -0300

    Sem10: Sistema de venta de pasajes

--- semana10/Pasajero.py
+++ semana10/Pasajero.py
@@ -1,12 +1,28 @@
 class Pasajero:
 
     def __init__(self) -> None:
+        pass
+    
+    def read(self) -> None:
         print("**** DATOS DEL PASAJERO ****")
         self.dni = input("DNI: ")
         self.nombres = input("Nombres: ")
         self.celular = input("Celular: ")
     
-    def grabar(self) -> None:
+    def save(self) -> None:
         f_pasajeros = open("pasajeros.txt", "a")
         f_pasajeros.write("{},{},{}\n".format(self.dni, self.nombres, self.celular))
         f_pasajeros.close()
+    
+    def format(self, cadena: str):
+        datos = cadena.split(",")
+        self.dni = datos[0]
+        self.nombres = datos[1]
+        self.celular = datos[2]
+        return self
+
+    def _print(self):
+        print("DNI: {} \tNombres: {}\tCelular:{}".format(self.dni, self.nombres, self.celular), end="")
+
+    def find(self, dni: str) -> bool:
+        return self.dni == dni
--- semana10/venta-pasajes.py
+++ semana10/venta-pasajes.py
@@ -2,10 +2,51 @@
 # Ingresar los pasajeros
 from Pasajero import Pasajero
 
-# Crear el archivo pasajeros
-file = open("pasajeros.txt", "w")
-file.close
+def main_menu():
+    print("***** MENU PRINCIPAL *****")
+    print("[1] Agregar Pasajero")
+    print("[2] Mostrar Pasajeros")
+    print("[3] Buscar Pasajero")
+    print("[4] Vender Pasaje")
+    print("[X] Salir")
 
-for i in range(5):
-    pasajero = Pasajero()
-    pasajero.grabar()
+def to_list_pasajeros():
+    pasajeros = read_pasajeros()
+    for pasaj in pasajeros:
+        pasaj._print()
+
+def read_pasajeros():
+    pasajeros = []
+    f_pasajeros = open("pasajeros.txt", "r")
+    lines = f_pasajeros.readlines()    
+    for line in lines:        
+        pasajero = Pasajero()
+        pasajeros.append(pasajero.format(line))
+    return pasajeros
+
+def find_pasajero(dni_search: str):
+    pasajeros = read_pasajeros()
+    for pasaj in pasajeros:
+        if (pasaj.find(dni_search)):
+            print("Pasajero encontrado")
+            pasaj._print()
+
+while True:
+    main_menu()
+    opc = input("Ingrese Opción: ")    
+    if opc == "1": 
+        pasajero = Pasajero()
+        pasajero.read()       
+        pasajero.save()
+    elif opc == "2":
+        print("Lista de Pasajeros")
+        to_list_pasajeros()
+    elif opc == "3":
+        dni_search = input("DNI a buscar: ")
+        find_pasajero(dni_search)
+    else:
+        break
+
+# Crear el archivo pasajeros
+# file = open("pasajeros.txt", "w")
+# file.close
