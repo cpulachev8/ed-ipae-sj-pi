535d63832108f0a19ffbc3b8e65b2015c7dae0a6
cpulachev8 <cpulachev8@gmail.com>
2021-12-22 23:19:09 -0300

    Sem11: Creación de dialog

--- semana11/main.py
+++ semana11/main.py
@@ -1,8 +1,17 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QDialog
+
+class DialogWelcome(QDialog):
+
+    def __init__(self) -> None:
+        super(DialogWelcome, self).__init__()
+        uic.loadUi("mi-primer-dialog.ui", self)
+        self.lbWelcome.setText("Bienvenido {}".format(form_gui.leUser.text()))
 
 def start():
     print("Click en el botón iniciar", form_gui.leUser.text())
+    dialogWelcome = DialogWelcome()
+    dialogWelcome.exec()
 
 Form, Window = uic.loadUiType("mi-primer-gui.ui")
 
