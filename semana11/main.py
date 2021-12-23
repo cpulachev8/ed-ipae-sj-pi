4dea24bad2dbe8d13f338f511a8c5ed4c92fad3c
cpulachev8 <cpulachev8@gmail.com>
2021-12-22 22:52:28 -0300

    Sem11: Qt Designer - uso de botón y caja de texto

--- semana11/main.py
+++ semana11/main.py
@@ -1,6 +1,9 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 
+def start():
+    print("Click en el botón iniciar", form_gui.leUser.text())
+
 Form, Window = uic.loadUiType("mi-primer-gui.ui")
 
 app_gui = QApplication([])
@@ -8,4 +11,5 @@ window_gui = Window()
 form_gui = Form()
 form_gui.setupUi(window_gui)
 window_gui.show()
+form_gui.pbStart.clicked.connect(start)
 app_gui.exec()
