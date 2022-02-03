cfee5c45b4bbf99e89ccaaf14124cbfef3bacbb9
cpulachev8 <cpulachev8@gmail.com>
2022-02-02 22:13:46 -0300

    Sem16: Unittest with coverage

---
+++ semana16/ValidacionesTest.py
@@ -0,0 +1,27 @@
+import unittest
+import Validaciones
+
+class OperacionesTest(unittest.TestCase):
+    # def test_RUC_valido(self):
+    #     self.assertEqual(Validaciones.validarRUC('20467534026'), True)
+
+    # def test_RUC_no_valido1(self):
+    #     self.assertEqual(Validaciones.validarRUC('20467534025'), False)
+
+    # def test_RUC_no_valido2(self):
+    #     self.assertNotEqual(Validaciones.validarRUC('20467534025'), True)
+
+    # def test_RUC_exception(self):
+    #     self.assertRaises(TypeError, Validaciones.validarRUC, 20467534026)
+           
+    def test_positive(self):
+        #ruc 10465988458  
+        #otro_ruc 20336951527
+        self.assertTrue( Validaciones.validarRUC('20512333797') is True)
+
+    def test_ruc_numeros_caracteres(self):
+        self.assertEqual(Validaciones.validarRUC("203938"), False)
+        
+ 
+if __name__ == '__main__':
+    unittest.main()
